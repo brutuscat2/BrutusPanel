cadb1b07a47e8b82f104f59a88e214d73fe1bece
brutuscat2 <michael_jarman@me.com>
2013-11-12 08:39:05 -0500

    Added login page

--- login/index.php
+++ login/index.php
@@ -44,13 +44,43 @@
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
+            <li><a href="/">Home</a></li>
             <li><a href="servers">Servers</a></li>
-            <li><a href="login">Login</a></li>
+            <li class="active"><a href="#">Login</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
     </div>
+
+   <form class="form-horizontal" role="form">
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmai" placeholder="Email">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> Remember me
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-primary">Sign in</button>
+    </div>
+  </div>
+</form>
+
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
