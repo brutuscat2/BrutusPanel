7606d5876b22d2539ebdb5e5d671fd348d188483
brutuscat2 <michael_jarman@me.com>
2013-11-12 13:15:57 -0500

    Added better login script

--- login/index.php
+++ login/index.php
@@ -54,15 +54,15 @@
 
    <form class="form-horizontal" type="post" action="login.php" role="form">
   <div class="form-group">
-    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+    <label for="user_name" class="col-sm-2 control-label">Usernameo</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmai" placeholder="Email">
+      <input type="email" class="form-control" id="user_name" placeholder="Email">
     </div>
   </div>
   <div class="form-group">
-    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+    <label for="user_password" class="col-sm-2 control-label">Password</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+      <input type="password" class="form-control" id="user_password" placeholder="Password">
     </div>
   </div>
   <div class="form-group">
