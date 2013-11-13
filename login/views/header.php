562f02ff480ad274a2b5fdec3f3fa7613a6d2304
brutuscat2 <michael_jarman@me.com>
2013-11-13 13:16:56 -0500

    Forgot to update header code for navbar...

--- login/views/header.php
+++ login/views/header.php
@@ -2,26 +2,31 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>PHP-login</title>
-<style type="text/css">
-    body {
-        font-family: Helvetica, Verdana, Arial, sans-serif;
-        font-size: 10pt;
-        margin: 10px;
-    }
-    label {
-        position: relative;
-        vertical-align: middle;
-        bottom: 1px;
-    }
-    input[type=text], input[type=password], input[type=submit], input[type=email] {
-        display: block;
-        margin-bottom: 15px;
-    }
-    input[type=checkbox] {
-        margin-bottom: 15px;
-    }
-</style>
+<title>Login</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="An Minecraft Control Panel">
+    <meta name="author" content="brutuscat2">
+    <link rel="shortcut icon" href="http://getbootstrap.com/docs-assets/ico/favicon.png">
+
+    <title>BrutusPanel</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="../css/bootstrap-theme.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../../css/theme.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  <style type="text/css" id="holderjs-style"></style></head>
 </head>
 <body>
 <?php
@@ -41,3 +46,26 @@ if ($login->messages) {
 }
 
 ?>   
+
+
+   <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">BrutusPanel</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="..">Home</a></li>
+            <li><a href="servers">Servers</a></li>
+            <li class="active"><a href="#">Login</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+	<br /><br /><br /><br />
